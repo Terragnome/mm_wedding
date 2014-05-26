1521ba525bf54b8456165edb5fb6c2006a55c605
mlin <mlin@kabam.com>
2014-05-25 21:20:26 -0700

    Changed db type, added routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def about
+  end
+  
+  def info
+  end
+  
+  def rsvp
+  end
+  
+  def registry
+  end
+  
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
   
+  get 'about' => 'home#about'
+  get 'info' => 'home#info'
+  get 'rsvp' => 'home#rsvp'
+  get 'registry' => 'home#registry'
+  get 'contact' => 'home#contact'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
