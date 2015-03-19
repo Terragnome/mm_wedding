8196712710e3f8eca1f21802e58c3716f99b2533
mlin <mlin@kabam.com>
2015-03-19 16:32:36 -0700

    Group paperclip photos by filename

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,9 @@ class Photo < ActiveRecord::Base
   has_attached_file :file,
     styles: {
       thumbnail: "128x128"
-    }
+    },
+    :url => "/system/:class/:attachment/:basename/:style/:basename.:extension",
+  	:path => ":rails_root/public/system/:class/:attachment/:basename/:style/:basename.:extension"
 
   validates_attachment :file, :presence => true, :size => { :in => 0..20.megabytes }
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
