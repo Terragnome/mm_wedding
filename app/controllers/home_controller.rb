da40098f4da58a1202090996386cf83c452fd1dd
mlin <mlin@kabam.com>
2015-03-19 20:02:47 -0700

    Sort photo results by filename

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
   end
 
   def photos
-    @ceremony_images = Photo.where(category: :ceremony)
-    @reception_images = Photo.where(category: :reception)
-    @outtake_images = Photo.where(category: :outtake)
+    @ceremony_images = Photo.where(category: :ceremony).order(:file_file_name)
+    @reception_images = Photo.where(category: :reception).order(:file_file_name)
+    @outtake_images = Photo.where(category: :outtake).order(:file_file_name)
   end
 
   def registry
